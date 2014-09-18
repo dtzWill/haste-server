5857331e5f44b011412f32963d59e3ce79824379
Will Dietz <w@wdtz.org>
2014-09-18 01:52:11 -0500

    run.sh: Only publish to localhost interface by default.

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ mkdir -p $DATA_DIR
 
 docker run -d -p 6379:6379 -v $DATA_DIR:/data --name redis dockerfile/redis
 
-docker run -p 7777:7777 --link redis:redis wdtz/hastebin
+docker run -p 127.0.0.1:7777:7777 --link redis:redis wdtz/hastebin
 
 docker stop redis
 docker rm redis
