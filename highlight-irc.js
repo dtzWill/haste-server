73f9e8fe539076ae7fbcc9c974dd911fd4c1429b
Will Dietz <w@wdtz.org>
2015-02-11 12:00:58 -0600

    Improve irc log styling, largely reworked.
    * Info message support
    * Notice message support
    * recognize nicks with highlight styling
    * recognize pm styling
    * recognize format used in log
    * update to latest highlightjs (see new commit id in filename)

--- highlight-irc.js
+++ highlight-irc.js
@@ -3,64 +3,76 @@ Language: irclog
 Author: Will Dietz <w@wdtz.org>
 Website: N/A
 Description: Logs produced by my irssi style/configuration.
-  "Inspired" by asciidoc language definition.
 */
 function(hljs) {
+  var WRAPPING_RE = /$\n(?! )/;
+  var LINKS = {
+    className: 'link_url',
+    begin: '(http|https|ftp|ftps|file|irc|image:?):\\S+'
+  };
+  var MESSAGE = {
+    end: WRAPPING_RE,
+    contains: [ LINKS ]
+  };
+  var AFTER_TIMESTAMP = {
+    className: 'after',
+    contains: [
+    {
+      className: 'info',
+      begin: / ==>/,
+      end: WRAPPING_RE
+      // TODO: recognize and style common info messages
+    },
+    {
+      className: 'nick_full',
+      // TODO: '>> nick <<' means this is a highlighted line, style differently?
+      begin: / (>>|<)/,
+      end: /(>|<<) /,
+      contains: [
+      {
+        className: 'nick_rank',
+        begin: '[\\+@& ]'
+      },
+      {
+        className: 'nick',
+        begin: /[^><\\+@& ]+/
+      }
+      ],
+      starts: MESSAGE
+    },
+    {
+      className: 'nick',
+      begin: / [^><=]\S*/,
+      starts: MESSAGE
+    }
+    ]
+  };
   return {
     aliases: ['irssi', 'irclog'],
     contains: [
-      // message prefix
-      // Matching this in one go avoids matching from message content
     {
-      className: 'message_prefix',
-      begin: '^\\.:',
-      end: />/,
-      returnBegin: true,
-      contains: [
-      // timestamp styling, timestamp within
-      {
-        className: 'timestamp_styled',
-        begin: '\\.:',
-        end: ':\\.',
-        contains: [
-        {
-          className: 'timestamp',
-          begin: '\\d\\d:\\d\\d:\\d\\d',
-          relevance: 50
-        }
-        ]
-      },
-
-      // nick
-      {
-        className: 'nick_full',
-        begin: /</,
-        end: />/,
-        returnBegin: true,
-        returnEnd: true,
-        contains: [
-        {
-          className: 'nick_rank',
-          begin: '<',
-          end: '(\\+| |\\@|\\&)',
-          excludeBegin: true
-        },
-        {
-          className: 'nick',
-          begin: hljs.IDENT_RE,
-          end: />/,
-          returnEnd: true
-        }
-        ]
-      }
-      ],
-      relevance: 10
+      className: 'timestamp',
+      begin: '^\\d\\d:\\d\\d(:\\d\\d)?(?!:)',
+      starts: AFTER_TIMESTAMP
     },
-    // Links
     {
-      className: 'link_url',
-      begin: '(http|https|ftp|ftps|file|irc|image:?):\\S+'
-    }
+      className: 'timestamp_styled',
+      begin: '^\\.:',
+      end: ':\\.',
+      contains: [ {
+        className: 'timestamp',
+        begin: '\\d\\d:\\d\\d:\\d\\d',
+        relevance: 50
+      } ],
+      starts: AFTER_TIMESTAMP
+    },
+    {
+      className: "header", // close enough
+      begin: /^---/,
+      end: WRAPPING_RE
+    },
+    // Match links outside of log entries:
+    LINKS
     ]
   };
 }
